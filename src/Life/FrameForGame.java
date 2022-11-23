1a3f262588c2715fecb40185252544c67be438be
vimark <stanislava1804@gmail.com>
2022-11-23 16:16:46 +0200

    fourth commit

--- src/Life/FrameForGame.java
+++ src/Life/FrameForGame.java
@@ -1,64 +1,124 @@
 package Life;
 
 import javax.swing.*;
+import javax.swing.plaf.ButtonUI;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import java.util.Random;
 
 public class FrameForGame extends JPanel{
     Random random = new Random();
 
-    JFrame frame;
-    JPanel panel;
-    JPanel panelForButtons;
-    JButton selectForm;
-    JButton buttonStop;
-    JButton buttonClear;
-    Cursor cursor = new Cursor(Cursor.HAND_CURSOR);
+    private final JFrame frame;
+    private JPanel panel;
+    private final JPanel panelForButtons;
+    private final JPanel panelForHelp;
+    private final JButton buttonStop;
+    private final JButton buttonClear;
+    private final JButton help;
+    private final JComboBox<String> forms;
+    private final JButton music;
+    private final Cursor cursor = new Cursor(Cursor.HAND_CURSOR);
+    private final String SONG;
 
-    int sizeOfY = 50;
-    int sizeOfX = 70;
-    int WIDTH = 1000, HEIGHT = 800;
-    boolean[][] isCellsAlive;
-    JPanel[][] oneCell;
+    private final int sizeOfY = 50;
+    private final int sizeOfX = 70;
+    private final int WIDTH = 1000;
+    private final int HEIGHT = 800;
+    private boolean[][] isCellsAlive;
+    private final JPanel[][] oneCell;
+    private final JLabel icon;
+    private final String pathToImage;
+    private final Music mu;
+    private XYfromFile xy;
 
     public FrameForGame() {
+        mu = new Music();
+        SONG = "Адажио Контабиле   Гайдн.wav";
+        mu.setFile(SONG);
+        mu.play();
+
         frame = new JFrame("Game of Life(Moskaliuk CS21)");
         isCellsAlive = new boolean[sizeOfY][sizeOfX];
         oneCell = new JPanel[sizeOfY][sizeOfX];
 
         panel = new JPanel();
         panel.setSize(WIDTH, HEIGHT);
-        panel.setBackground(Color.gray);
-        panel.setLayout(new GridLayout(sizeOfY, sizeOfX));
+        icon = new JLabel();
+        pathToImage = "StartFrame.png";
+        icon.setIcon(new ImageIcon(pathToImage));
+        panel.add(icon, BorderLayout.CENTER);
         frame.add(panel);
 
         panelForButtons = new JPanel();
         panelForButtons.setBackground(Color.black);
+        panelForHelp = new JPanel();
 
-        buttonStop = new JButton("Stop");
-        selectForm = new JButton("One cell");
-        buttonClear = new JButton("Start game");
+        forms = new JComboBox<>();
+        forms.addItem("One cell");
+        forms.addItem("3 cells");
+        forms.addItem("12 cells");
+        forms.addItem("Glider");
+        forms.addItem("Spaceship");
+        forms.addItem("Oscillator");
+        forms.addItem("Blinker Fuse");
 
-        buttonStop.setCursor(cursor);
-        selectForm.setCursor(cursor);
-        buttonClear.setCursor(cursor);
-
-        panelForButtons.add(selectForm);
-        panelForButtons.add(buttonStop);
-        panelForButtons.add(buttonClear);
-
-        selectForm.addActionListener(e -> {
-            if("Glider".equalsIgnoreCase(String.valueOf(selectForm.getText()))){
-                selectForm.setText("One cell");
-            } else if("One cell".equalsIgnoreCase(String.valueOf(selectForm.getText()))){
-                selectForm.setText("3 cells");
-            } else if("3 cells".equalsIgnoreCase(String.valueOf(selectForm.getText()))){
-                selectForm.setText("12 cells");
-            } else if("12 cells".equalsIgnoreCase(String.valueOf(selectForm.getText()))){
-                selectForm.setText("Glider");
+        music = new JButton("Stop music");
+        music.addActionListener(e ->{
+            if("Stop music".equalsIgnoreCase(String.valueOf(music.getText()))){
+                music.setText("Start music");
+                try {
+                    mu.stop();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            } else {
+                music.setText("Stop music");
+                mu.setFile(SONG);
+                mu.play();
             }
         });
+
+        buttonStop = new JButton("Stop life");
+        buttonClear = new JButton("Start game");
+        help = new JButton("Help");
+        help.setBorderPainted(false);
+        help.setBackground(Color.lightGray);
+        help.setFocusPainted(false);
+
+        help.setCursor(cursor);
+        buttonStop.setCursor(cursor);
+        forms.setCursor(cursor);
+        buttonClear.setCursor(cursor);
+
+        help.addActionListener(e ->{
+            JOptionPane.showMessageDialog(new JFrame(), "Правила гри: " +
+                    "\n\n1. Якщо клітина має менше 2 сусідів - вона помирає від самотності" +
+                    "\n2. Якщо клітина має більше 3 сусідів - вона помирає від перенаселення" +
+                    "\n3. Якщо клітина має 2 або 3 сусіди - вона лишається живою" +
+                    "\n4. Нова клітина створюється, якщо в неї є рівно 3 сусіди" +
+                    "\n\nЗначення кнопок:" +
+                    "\n\"Start game\" - розпочинає гру та вмикає фонову музику" +
+                    "\n\"Clear\" - заповнює пусте поле без клітин" +
+                    "\n\"Random fill\" - заповнює поле з рандомним розташуванням клітин" +
+                    "\n\"Stop music\" та \"Start music\" - зупиняє та вмикає заново фонову музику, відповідно" +
+                    "\n\"Stop life\" та \"Start life\" - зупиняє та вмикає процес гри" +
+                    "\nКомбіноване меню з різними назвами форм - створює на полі обраний вами набір клітинок:" +
+                    "\n1) \"One cell\" - одна клітина" +
+                    "\n2) \"3 cells\" - 3 розташовані вертикально клітини, що утворюють разом блінкер(осцилятор)" +
+                    "\n3) \"12 cells\" - 12 клітин" +
+                    "\n4) \"Glider\" - рухомий у рандомному напрямку глайдер" +
+                    "\n5) \"Spaceship\" - рухомий космічний корабель" +
+                    "\n6) \"Oscillator\" - нерухомий осцилятор" +
+                    "\n7) \"Blinker Fuse\" - рухомий блінкер ф'юз, що \"їсть\" 3-клітинні осцилятори(блінкери)");
+        });
+
+        panelForButtons.add(buttonClear);
+        panelForButtons.add(buttonStop);
+        panelForButtons.add(music);
+        panelForButtons.add(forms);
+        panelForHelp.add(help);
 
         Timer timer = new Timer(100, e -> {
             boolean[][] temp = new boolean[sizeOfY][sizeOfX];
@@ -92,14 +152,13 @@ public class FrameForGame extends JPanel{
             }
         });
 
-
         buttonStop.addActionListener(e -> {
-            if("Stop".equalsIgnoreCase(String.valueOf(buttonStop.getText()))){
+            if("Stop life".equalsIgnoreCase(String.valueOf(buttonStop.getText()))){
                 timer.stop();
-                buttonStop.setText("Start");
+                buttonStop.setText("Start life");
             } else {
                 timer.start();
-                buttonStop.setText("Stop");
+                buttonStop.setText("Stop life");
             }
         });
 
@@ -107,7 +166,7 @@ public class FrameForGame extends JPanel{
             frame.remove(panel);
 
             timer.start();
-            buttonStop.setText("Stop");
+            buttonStop.setText("Stop life");
 
             panel = new JPanel();
             panel.setSize(WIDTH, HEIGHT);
@@ -115,7 +174,8 @@ public class FrameForGame extends JPanel{
             panel.setLayout(new GridLayout(sizeOfY, sizeOfX));
             frame.add(panel);
 
-            if("Random fill".equalsIgnoreCase(String.valueOf(buttonClear.getText())) || "Start game".equalsIgnoreCase(String.valueOf(buttonClear.getText()))){
+            if("Random fill".equalsIgnoreCase(String.valueOf(buttonClear.getText())) ||
+                    "Start game".equalsIgnoreCase(String.valueOf(buttonClear.getText()))){
                 buttonClear.setText("Clear");
                 for (int i = 0; i < sizeOfY; i++) {
                     for (int j = 0; j < sizeOfX; j++) {
@@ -151,87 +211,104 @@ public class FrameForGame extends JPanel{
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                int x = (int) ((e.getX() - 10)/14.1);
-                int y = (e.getY() - 40)/14;
-                if("One cell".equalsIgnoreCase(String.valueOf(selectForm.getText()))) {
-                    if (isCellsAlive[y][x]) {
-                        oneCell[y][x].setBackground(Color.gray);
-                        isCellsAlive[y][x] = false;
-                    } else {
-                        oneCell[y][x].setBackground(Color.yellow);
-                        isCellsAlive[y][x] = true;
-                    }
-                } else if("3 cells".equalsIgnoreCase(String.valueOf(selectForm.getText()))) {
-                    int i = y - 1;
-                    while (i < y + 2) {
-                        if (i < sizeOfY && i >= 0) {
-                            oneCell[i][x].setBackground(Color.yellow);
-                            isCellsAlive[i][x] = true;
-                        }
-                        i++;
-                    }
-                }else if("12 cells".equalsIgnoreCase(String.valueOf(selectForm.getText()))){
-                    int i = y - 3;
-                    int j = x - 3;
-                    while(i < y + 4) {
-                        if(i < sizeOfY && i >= 0) {
-                            if (i == y) {
-                                oneCell[i][x].setBackground(Color.gray);
-                                isCellsAlive[i][x] = false;
-                            } else {
-                                oneCell[i][x].setBackground(Color.yellow);
-                                isCellsAlive[i][x] = true;
-                            }
-                        }
-                        i++;
-                    }
-                    while(j < x + 4) {
-                        if(j < sizeOfX && j >= 0) {
-                            if (j == x) {
-                                oneCell[y][j].setBackground(Color.gray);
-                                isCellsAlive[y][j] = false;
-                            } else {
-                                oneCell[y][j].setBackground(Color.yellow);
-                                isCellsAlive[y][j] = true;
-                            }
-                        }
-                        j++;
-                    }
-                } else if("Glider".equalsIgnoreCase(String.valueOf(selectForm.getText()))) {
-                    int vertical = random.nextInt(1, 3);
-                    int horizontal = random.nextInt(1, 3);
-                    if ((x >= 2 && x <= sizeOfX-3) && (y >= 2 && y <= sizeOfY-3)) {
-                        if (vertical == 1) {
-                            for (int i = y; i >= y - 2; i--) {
-                                oneCell[i][x].setBackground(Color.yellow);
-                                isCellsAlive[i][x] = true;
-                            }
-                            if (horizontal == 1) {
-                                for (int j = 0; j < 2; j++) {
-                                    oneCell[y - j][x - 1 - j].setBackground(Color.yellow);
-                                    isCellsAlive[y - j][x - 1 - j] = true;
-                                }
-                            } else {
-                                for (int j = 0; j < 2; j++) {
-                                    oneCell[y - j][x + 1 + j].setBackground(Color.yellow);
-                                    isCellsAlive[y - j][x + 1 + j] = true;
-                                }
-                            }
+                int x = (int) ((e.getX() - 10) / 14.1);
+                int y = (e.getY() - 85) / 13;
+                if (!"Start game".equalsIgnoreCase(buttonClear.getText()) && (y >= 0 && y < 50)) {
+                    if ("One cell".equalsIgnoreCase(String.valueOf(forms.getSelectedItem()))) {
+                        if (isCellsAlive[y][x]) {
+                            oneCell[y][x].setBackground(Color.gray);
+                            isCellsAlive[y][x] = false;
                         } else {
-                            for (int i = y; i <= y + 2; i++) {
+                            oneCell[y][x].setBackground(Color.yellow);
+                            isCellsAlive[y][x] = true;
+                        }
+                    } else if ("3 cells".equalsIgnoreCase(String.valueOf(forms.getSelectedItem()))) {
+                        int i = y - 1;
+                        while (i < y + 2) {
+                            if (i < sizeOfY && i >= 0) {
                                 oneCell[i][x].setBackground(Color.yellow);
                                 isCellsAlive[i][x] = true;
                             }
-                            if (horizontal == 1) {
-                                for (int j = 0; j < 2; j++) {
-                                    oneCell[y + j][x - 1 - j].setBackground(Color.yellow);
-                                    isCellsAlive[y + j][x - 1 - j] = true;
+                            i++;
+                        }
+                    } else if ("12 cells".equalsIgnoreCase(String.valueOf(forms.getSelectedItem()))) {
+                        int i = y - 3;
+                        int j = x - 3;
+                        while (i < y + 4) {
+                            if (i < sizeOfY && i >= 0) {
+                                if (i == y) {
+                                    oneCell[i][x].setBackground(Color.gray);
+                                    isCellsAlive[i][x] = false;
+                                } else {
+                                    oneCell[i][x].setBackground(Color.yellow);
+                                    isCellsAlive[i][x] = true;
+                                }
+                            }
+                            i++;
+                        }
+                        while (j < x + 4) {
+                            if (j < sizeOfX && j >= 0) {
+                                if (j == x) {
+                                    oneCell[y][j].setBackground(Color.gray);
+                                    isCellsAlive[y][j] = false;
+                                } else {
+                                    oneCell[y][j].setBackground(Color.yellow);
+                                    isCellsAlive[y][j] = true;
+                                }
+                            }
+                            j++;
+                        }
+                    } else if ("Glider".equalsIgnoreCase(String.valueOf(forms.getSelectedItem()))) {
+                        int vertical = random.nextInt(1, 3);
+                        int horizontal = random.nextInt(1, 3);
+                        if ((x >= 2 && x <= sizeOfX - 3) && (y >= 2 && y <= sizeOfY - 3)) {
+                            if (vertical == 1) {
+                                for (int i = y; i >= y - 2; i--) {
+                                    oneCell[i][x].setBackground(Color.yellow);
+                                    isCellsAlive[i][x] = true;
+                                }
+                                if (horizontal == 1) {
+                                    for (int j = 0; j < 2; j++) {
+                                        oneCell[y - j][x - 1 - j].setBackground(Color.yellow);
+                                        isCellsAlive[y - j][x - 1 - j] = true;
+                                    }
+                                } else {
+                                    for (int j = 0; j < 2; j++) {
+                                        oneCell[y - j][x + 1 + j].setBackground(Color.yellow);
+                                        isCellsAlive[y - j][x + 1 + j] = true;
+                                    }
                                 }
                             } else {
-                                for (int j = 0; j < 2; j++) {
-                                    oneCell[y + j][x + 1 + j].setBackground(Color.yellow);
-                                    isCellsAlive[y + j][x + 1 + j] = true;
+                                for (int i = y; i <= y + 2; i++) {
+                                    oneCell[i][x].setBackground(Color.yellow);
+                                    isCellsAlive[i][x] = true;
                                 }
+                                if (horizontal == 1) {
+                                    for (int j = 0; j < 2; j++) {
+                                        oneCell[y + j][x - 1 - j].setBackground(Color.yellow);
+                                        isCellsAlive[y + j][x - 1 - j] = true;
+                                    }
+                                } else {
+                                    for (int j = 0; j < 2; j++) {
+                                        oneCell[y + j][x + 1 + j].setBackground(Color.yellow);
+                                        isCellsAlive[y + j][x + 1 + j] = true;
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        if ("Spaceship".equalsIgnoreCase(String.valueOf(forms.getSelectedItem()))) {
+                            xy = new XYfromFile("spaceship.txt");
+                        } else if ("Oscillator".equalsIgnoreCase(String.valueOf(forms.getSelectedItem()))) {
+                            xy = new XYfromFile("oscillator1.txt");
+                        } else if ("Blinker Fuse".equalsIgnoreCase(String.valueOf(forms.getSelectedItem()))) {
+                            xy = new XYfromFile("blinker_fuse.txt");
+                        }
+                        for (int i = 0; i < xy.arrayX.size(); i++) {
+                            if ((y + xy.getArrayY().get(i) > 0 && y + xy.getArrayY().get(i) < sizeOfY) &&
+                                    (x + xy.getArrayX().get(i) > 0 && x + xy.getArrayX().get(i) < sizeOfX)) {
+                                    oneCell[y + xy.getArrayY().get(i)][x + xy.getArrayX().get(i)].setBackground(Color.yellow);
+                                    isCellsAlive[y + xy.getArrayY().get(i)][x + xy.getArrayX().get(i)] = true;
                             }
                         }
                     }
@@ -239,7 +316,8 @@ public class FrameForGame extends JPanel{
             }
         });
 
-        frame.add(panelForButtons, BorderLayout.PAGE_END);
+        frame.add(panelForButtons, BorderLayout.PAGE_START);
+        frame.add(panelForHelp, BorderLayout.PAGE_END);
         frame.setSize(WIDTH, HEIGHT);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
---
+++ src/Life/XYfromFile.java
@@ -0,0 +1,43 @@
+package Life;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class XYfromFile {
+    ArrayList<Integer> arrayX = new ArrayList<>();
+    ArrayList<Integer> arrayY = new ArrayList<>();
+    String fileName;
+
+    public XYfromFile(String fileName) {
+        this.fileName = fileName;
+        openFile(fileName);
+    }
+
+    public void openFile(String fileName){
+        try (BufferedReader br1 = new BufferedReader(new FileReader(fileName))) {
+            String str;
+            String[] arr;
+            int i = 0;
+            while ((str = br1.readLine()) != null){
+                arr = str.split(" ");
+                arrayY.add(i, Integer.parseInt(arr[0]));
+                arrayX.add(i, Integer.parseInt(arr[1]));
+                i++;
+            }
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public ArrayList<Integer> getArrayX() {
+        return arrayX;
+    }
+
+    public ArrayList<Integer> getArrayY() {
+        return arrayY;
+    }
+}
